e713b746a5e532e6773e5dd99f7a927294f87f2d
AbdulMaaz909 <155896522+AbdulMaaz909@users.noreply.github.com>
2024-08-10 12:03:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# write a program that generates  as random number and asks the users to guess it.
-# if the player's guess is higher than the actual number, then 
-# program displays "lower number please".Similarly if the user guess is too low,the program prints "Higher number please" When the user guess
-#the correct number,the program display the number of guesses the player used to arrive at the number
-
 import random
 n=random.randint(1,100)
 a = -1
